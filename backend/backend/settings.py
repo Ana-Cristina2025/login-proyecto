5ad85caf8147a146147cf5e4ffa589c78c2805c1
Cristina Rojas <cristina1812@gmail.com>
2025-07-10 16:50:34 -0600

    Termine Back

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'loginapp',
     'rest_framework',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
